aaa30b34cb64fe5bd10280e30fd982927c77608e
AIO <69240351+aiomayo@users.noreply.github.com>
2025-01-05 21:55:21 +0100

    web

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,16 +5,14 @@
         <span class="text-sm text-gray-500 dark:text-gray-400">© 2025 <a href="https://dc.dayquest.de/" class="hover:underline">DayQuest</a>. All Rights Reserved.</span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+            <a href="/dayquest_impressum.txt" class="hover:underline me-4 md:me-6">Imprint</a>
           </li>
           <li>
             <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
           </li>
+
           <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Contact</a>
+            <a href="https://dc.dayquest.de" class="hover:underline">Contact</a>
           </li>
         </ul>
       </div>
